eab522655b3a67805214f46522c18a820de12559
Cengizism <culusoy@ask-cs.com>
2014-06-14 20:42:53 +0200

    working version with real wadl data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,80 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name restApp
- * @description
- * # restApp
- *
- * Main module of the application.
- */
-angular
-  .module('restApp', [
+var app = angular.module(
+  'restApp', [
     'ngResource',
     'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'mainController'
-      })
-      .when('/intern', {
-        templateUrl: 'views/intern.html',
-        controller: 'internController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  ]
+);
+
+app.
+  factory(
+  'ApplicationWadl',
+  [
+    '$http',
+    function ($http)
+    {
+      return {
+        get: function (callback)
+        {
+          $http.get(
+            'http://dev.ask-cs.com/application.wadl',
+            {
+              transformResponse: function (data)
+              {
+                var xmlParser = new X2JS();
+                data = xmlParser.xml_str2json(data);
+
+                var _paths = data.application.resources.resource,
+                    paths = {};
+
+                angular.forEach(
+                  _paths,
+                  function (node)
+                  {
+                    node.broken = node._path.split('/');
+
+                    node.broken.shift();
+
+                    if (! paths.hasOwnProperty(node.broken[0]))
+                    {
+                      paths[node.broken[0]] = [];
+                    }
+
+                    paths[node.broken[0]].push(node);
+                  }
+                );
+
+                return paths;
+              }
+            }
+          ).success(
+            function (data) { callback(data) }
+          );
+        }
+      }
+    }
+  ]
+);
+
+app.run(
+  [
+    '$rootScope', 'ApplicationWadl',
+    function ($rootScope, ApplicationWadl)
+    {
+      $rootScope.setPath = function (path)
+      {
+        $rootScope.path = $rootScope.paths[path];
+      };
+
+      ApplicationWadl.get(
+        function (paths)
+        {
+          $rootScope.paths = paths;
+
+          $rootScope.setPath($rootScope.paths[0]);
+        }
+      );
+    }
+  ]
+);
